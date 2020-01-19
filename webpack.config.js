a99787cf19eb69250118196466f8b41dce975bae
Ludwig <richter@fliegwerk.com>
2020-01-19 02:03:35 +0100

    Add boilerplate code and config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    externals: [ 'chalk' ]
+};
